097ae07ceaafd3db40ea25bb45c95b0b8d04cb77
jeholmajing <jeholmajing@didiglobal.com>
2019-01-27 11:05:55 +0800

    +selectsort

--- sort.js
+++ sort.js
@@ -14,35 +14,51 @@
 //
 // };
 
+//有问题
+// const doubleBubbleSort = (arr) => {
+// 	let low = 0, high = arr.length - 1;
+// 	while (low < high) {
+//
+// 		for (let i = low; i < high; i++) {
+// 			if (arr[i] > arr[i + 1]) {
+// 				let t = arr[i];
+// 				arr[i] = arr[i + 1];
+// 				arr[i + 1] = t;
+// 			}
+// 			high--;
+// 		}
+//
+// 		for (let i = high; i > low; i--) {
+// 			if (arr[i] < arr[i - 1]) {
+// 				let t = arr[i];
+// 				arr[i] = arr[i - 1];
+// 				arr[i - 1] = t;
+// 			}
+// 			low++;
+// 		}
+// 	}
+//
+//
+// 	return arr;
+// };
 
-const doubleBubbleSort = (arr) => {
-	let low = 0, high = arr.length - 1;
-	while (low < high) {
+const selectSort = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		let low = i;
 
-		for (let i = low; i < high; i++) {
-			if (arr[i] > arr[i + 1]) {
-				let t = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = t;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[low]) {
+				low = j;
 			}
-			high--;
-		}
 
-		for (let i = high; i > low; i--) {
-			if (arr[i] < arr[i - 1]) {
-				let t = arr[i];
-				arr[i] = arr[i - 1];
-				arr[i - 1] = t;
-			}
-			low++;
 		}
+		let t = arr[low];
+		arr[low] = arr[i];
+		arr[i] = t;
 	}
-
-
 	return arr;
 };
 
-
 const arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 
-console.log(doubleBubbleSort(arr));
+console.log(selectSort(arr));
